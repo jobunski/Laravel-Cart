998aab4607d178d77381ebe2d401192172e6c4bf
jobunski <jonyango003@gmail.com>
2019-08-16 18:38:51 +0300

    Populating Product page

---
+++ resources/views/users/signup.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.master')
+@section('title')
+    Shopping Cart
+@endsection
+@section('content')
+    @foreach($products->chunk(3) as $productChunk)
+        <div class="row">
+            @foreach($productChunk as $product)
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{ $product-> imagePath }}"  class="img-fluid">
+                        <div class="caption">
+                            <h3>{{ $product -> title }}</h3>
+                            <p class="description">{{ $product-> description }}</p>
+                            <div class="clearfix">
+                                <p class="price float-left">Ksh {{ $product-> price }}</p>
+                                <a href="/add-to-cart/" class="btn btn-success float-right" role="button">
+                                    Add
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @endforeach
+@endsection
